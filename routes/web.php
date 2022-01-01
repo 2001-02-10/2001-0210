460082f771a5d5a19fe7e3066dde183db76504ae
2001-02-10 <yuadiuno208@gmail.com>
2022-01-01 12:06:19 +0900

    forth commit

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,9 @@ use Illuminate\Support\Facades\Routes;
 */
 
 Route::get('/', 'PostController@index');
+
+Route::get('/posts/create', 'PostController@create');
+
 Route::get('/posts/{post}', 'PostController@show');
+
+
