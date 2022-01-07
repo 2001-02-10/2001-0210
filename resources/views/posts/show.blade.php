f031497a8e767f0d2a79a0443b3ac8473c663665
2001-02-10 <yuadiuno208@gmail.com>
2022-01-07 21:20:56 +0900

    final commit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,11 +8,26 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-                <div class='post'>
+         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+         <form action="/posts/{{ $post->id }}" id="form_delete" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" style="display:none">
+                    <button class="delete">[<span onclick="return deletePost(event);">delete</span>]</button>
+         </form>
+         <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                     <p class='updated_at'>{{ $post->updated_at }}</p>
-                </div>
-                <div class='back'>[<a href='/'>back</a>]</div>
+         </div>
+         <div class='back'>[<a href='/'>back</a>]</div>
+         <script>
+             function deletePost(e){
+             'use strict';
+             if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+             document.getElementById('form_delete').submit();
+             }
+            }
+        </script>
     </body>
 </html>
